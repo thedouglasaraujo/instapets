11fbb4ae1de8b2f714b5311e23660560928156e1
Douglas Silva <thedouglasilva@gmail.com>
2024-01-05 20:07:53 -0300

    feat: login and sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
-
-function HomePage() {
-  return <h1>Welcome to Monitoria de Frontend 2023.1</h1>;
-}
+import NewPostPage from "./pages/NewPost";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/newpost" element={<NewPostPage />} />
+    </Routes>
   );
 }
-
-export default App;
---
+++ src/components/Login/index.tsx
@@ -0,0 +1,57 @@
+import "./styles.css";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+interface LoginProps {
+  loginType: "login" | "signup" | undefined;
+}
+
+function Login({ loginType }: LoginProps) {
+  const [login, setLogin] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleChangeLogin = (e: ChangeEvent<HTMLInputElement>) => {
+    setLogin(e.target.value);
+  };
+
+  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLogin("");
+    setPassword("");
+  };
+
+  return (
+    <div className="login-container">
+      <h1>{loginType === "login" ? "Login" : "Criar conta"}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="login"
+          placeholder="Login"
+          value={login}
+          onChange={handleChangeLogin}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Senha"
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <input
+          type="submit"
+          value={loginType === "login" ? "Entrar" : "Criar conta"}
+        />
+      </form>
+      <NavLink to={loginType === "login" ? "/signup" : "/login"}>
+        {loginType === "login" ? "Criar conta" : "Já tenho conta"}
+      </NavLink>
+    </div>
+  );
+}
+
+export default Login;
