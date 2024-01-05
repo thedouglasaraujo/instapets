80741f811e9b126b8a4530f5dca9c8731c6245db
Douglas Silva <thedouglasilva@gmail.com>
2024-01-05 20:33:00 -0300

    fix: new post page

--- src/pages/NewPost/index.tsx
+++ src/pages/NewPost/index.tsx
@@ -1,5 +1,46 @@
-import NewPostButton from "../../components/NewPostButton";
+import { useState } from "react";
+import Modal from "../../components/Modal";
+import Post from "../../components/Post";
+import AddIcon from '@material-ui/icons/Add';
+import "./styles.css";
+
+interface PostData {
+    caption: string;
+    image: string | ArrayBuffer | null;
+}
 
 export default function NewPostPage() {
-  return <NewPostButton />;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [newPostData, setNewPostData] = useState<PostData | null>(null);
+
+    const handleOpenModal = () => {
+        setNewPostData(null);
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = (postData: PostData | null) => {
+        setIsModalOpen(false);
+        if (postData && (postData.image || postData.caption.trim() !== '')) {
+            setNewPostData(postData);
+        }
+    };
+
+    return (
+        <div className="new-post">
+            <button
+                className="add-post-button"
+                onClick={handleOpenModal}
+                disabled={isModalOpen}
+            >
+                Criar novo post <span><AddIcon /></span>
+            </button>
+            <div className="modal">
+                {isModalOpen && <Modal onClose={handleCloseModal} />}
+            </div>
+
+            {newPostData && (newPostData.image || newPostData.caption.trim() !== '') && (
+                <Post postData={newPostData} />
+            )}
+        </div>
+    );
 }
